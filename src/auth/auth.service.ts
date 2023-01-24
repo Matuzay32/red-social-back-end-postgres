9673afb9ebf08e11c97368e47358730c3ff8a257
linuxlite <eze.rey92@gmail.com>
2023-01-24 14:35:52 -0300

    dsaklsakd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,23 +11,11 @@ export class AuthService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
-  create(createAuthDto: CreateAuthDto) {
+  createUser(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
 
-  findAll() {
+  login() {
     return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
   }
 }
